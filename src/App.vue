8ff9be9d59018b8e7d39ff71db55e22bdc6ec84b
xfy196 <xfy196@outlook.com>
2021-03-19 00:03:42 +0800

    修复data一直处于监听状态问题

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,126 @@
 <template>
-	<VirtualListScroll :data="[]" :pageMode="false"></VirtualListScroll>
+    <div>
+        <div class="header">Virtual Block</div>
+        <div class="userOptions">
+            <span class="option">
+                <span>
+                    Data Amount
+                </span>
+                <select v-model="dataAmt">
+                    <option value="100">100</option>
+                    <option value="1000">1000</option>
+                    <option value="10000">10000</option>
+                    <option value="200000">200000</option>
+                    <option value="500000">500000</option>
+                </select>
+            </span>
+            <span class="option">
+                <span>
+                    Page Mode
+                </span>
+                <input type="checkbox" v-model="isPageMode">
+            </span>
+            <span class="option">
+                <span>
+                    Fixed Block Height
+                </span>
+                <input type="checkbox" v-model="isFixedHeight">
+            </span>
+        </div>
+        <VirtualListScroll :fixedBlockHeight="isFixedHeight ? 50 : undefined" v-if="true" :pageMode="isPageMode" :height="500" :data="data" ref="vb">
+            <template slot-scope="{data}">
+                <div :style="{height: '100%', 'background-color': data.color}">
+                    {{data.id}}
+                </div>
+            </template>
+        </VirtualListScroll>
+        <div class="footer">
+            <span>
+                🌈#supportlgbt :)
+            </span>
+        </div>
+    </div>
 </template>
 
 <script>
-import Vue from "vue"
-import VirtualListScroll from "./index.js"
+import VirtualListScroll from "./index";
+import Vue from "vue";
 Vue.use(VirtualListScroll)
 export default {
-
-  name: 'App',
-
-  data () {
-    return {
-
+    name: "App",
+    data() {
+        return {
+            data: [],
+            dataAmt: '1000',
+            isPageMode: false,
+            isFixedHeight: true
+        }
+    },
+    created() {
+        this.data = this.dataConstructor(this.dataAmt, this.isFixedHeight);
+    },
+    watch: {
+        dataAmt: function() {
+            this.data = this.dataConstructor(this.dataAmt, this.isFixedHeight);
+            this.$forceUpdate();
+        },
+        isPageMode: function() {
+            this.$forceUpdate();
+        },
+        isFixedHeight: function() {
+            this.data = this.dataConstructor(this.dataAmt, this.isFixedHeight);
+            this.$forceUpdate();
+        }
+    },
+    computed: {
+    },
+    methods: {
+        dataConstructor(amount, fixedHeight) {
+            let constructedArr = [];
+            for (let i = 0 ; i < Number(amount) ; i++) {
+                let constructedObj = {};
+                constructedObj['height'] = fixedHeight ? 50 : this.randomInteger(30, 90);
+                constructedObj['id'] = i;
+                constructedObj['color'] = '#' + this.randomColor();
+                constructedArr.push(constructedObj);
+            }
+            return constructedArr;
+        },
+        randomColor() {
+            return Math.floor(Math.random() * 16777215).toString(16);
+        },
+        randomInteger(min, max) {
+            return Math.floor(Math.random() * (max - min + 1) ) + min;
+        }
     }
-  }
 }
 </script>
 
-<style lang="css" scoped>
+<style scoped>
+span, div{
+    font-family: 'Helvetica Neue', sans-serif;
+}
+
+.header {
+    font-family: 'Helvetica Neue', sans-serif;
+    font-size: 25px;
+    text-align: center;
+}
+
+.userOptions {
+    margin: 10px 0;
+}
+
+.option {
+    margin: 0 7px;
+}
+
+.footer {
+    font-size: 10px;
+    text-align: center;
+}
+
+.btn {
+    cursor: pointer;
+}
 </style>
--- src/VirtualListScroll/index.vue
+++ src/VirtualListScroll/index.vue
@@ -1,5 +1,15 @@
 <template>
-  <div :style="containerStyle" ref="vb"></div>
+  <div v-on="pageMode ? {} : {scroll: handleScroll}" :style="containerStyle" ref="vb">
+    <div :style="{height: `${offsetTop}px`}"></div>
+    <div
+      :style="{height: `${flxedBlockHeight? flxedBlockHeight: item.height}px`}"
+      v-for="item in renderList"
+      :key="item.id"
+    >
+      <slot :data="item"></slot>
+    </div>
+    <div :style="{height: `${offsetBot}px`}"></div>
+  </div>
 </template>
 
 <script>
@@ -8,21 +18,64 @@ export default {
     // 具体的列表的数组
     data: {
       type: Array,
-      required: true,
+      required: true
     },
     // 虚拟块的高度
     height: {
-      type: Number,
+      type: Number
     },
     // 虚拟滚动去每一个item的高度
-    flexdBlockHeight: {
-      type: Number,
+    flxedBlockHeight: {
+      type: Number
     },
     // 假定是window有滚动条还是虚拟滚动区有滚动条
     pageMode: {
       type: Boolean,
-      required: true,
+      required: true
+    }
+  },
+  data() {
+    return {
+      renderList: [],
+      viewportEnd: this.height,
+      viewportBegin: 0,
+      offsetTop: 0,
+      offsetBot: 0,
+      transformedData: []
+    };
+  },
+  watch: {
+    data: {
+      handler: function(newValue, oldValue) {
+        this.computeTransformedData(newValue)
+        // 如果存在旧值
+        if (oldValue) {
+          this.$nextTick(() => {
+            this.$refs.vb.scrollTop = 0;
+            this.handleScroll();
+          });
+        }
+      },
+      immediate: true
     },
+    pageMode(newValue) {
+      if (newValue) {
+        window.addEventListener("scroll", this.handleScroll);
+      } else {
+        // 如果不存在新的之变化直接清楚这个scorll的滚动监听
+        window.removeEventListener("scroll", this.handleScroll);
+      }
+      // 值发生变化直接需要重新计算
+      this.computeTransformedData(this.data);
+      this.$nextTick(() => {
+        this.$refs.vb.scrollTop = 0;
+        this.handleScroll();
+      });
+    },
+    flxedBlockHeight() {
+      //重新调用滚动事件的方法
+      this.handleScroll();
+    }
   },
   created() {
     // 判断页面的模式
@@ -36,7 +89,7 @@ export default {
     }
     this.updateVb(0);
   },
-  destory() {
+  destroyed() {
     // 组价销毁时清除之前的事件
     if (this.pageMode) {
       window.removeEventListener("scroll", this.handleScroll);
@@ -45,13 +98,13 @@ export default {
   methods: {
     computeTransformedData(oldArr) {
       if (
-        (!this.flexdBlockHeight && this.pageMode && this.$refs.vb) ||
+        (!this.flxedBlockHeight && this.pageMode && this.$refs.vb) ||
         !this.pageMode
       ) {
         let curHeight = this.pageMode ? this.$refs.vb.offsetTop : 0;
         // 每一次的虚拟列表的第一个的scrolTop
         let rt = [curHeight];
-        oldArr.forEach((item) => {
+        oldArr.forEach(item => {
           curHeight += item.height;
           // 每一次计算后端的scrollTop
           rt.push(curHeight);
@@ -68,11 +121,14 @@ export default {
         ? window.pageYOffset
         : this.$refs.vb.scrollTop;
       // 为了必要滚动过快需要使用动画帧
-      window.reuquestAnimationFrame(() => {
+      window.requestAnimationFrame(() => {
         // 更新虚拟滚动区内的内容
         this.updateVb(scrollTop);
       });
     },
+    /**
+     * 存在指定的盒子高度寻找开始的下表
+     */
     findBlockHeightLowerBound(viewportBegin, flxedBlockHeight) {
       const sAdjusted = this.pageMode
         ? viewportBegin - this.$refs.scrollTop
@@ -80,16 +136,115 @@ export default {
       const computedStartIndex = ~~(sAdjusted / flxedBlockHeight);
       return computedStartIndex >= 0 ? computedStartIndex : 0;
     },
+    /**
+     * 不存在盒子指定的高度二分法寻找开始的下表
+     */
     binarySearchLowerBound(viewportBegin, transformedData) {
-      console.log(viewportBegin, transformedData);
+      let lo = 0;
+      // 最后一个高度的前一个其实就代表最后一个的头部位置
+      let hi = transformedData.length - 1;
+      let mid;
+      while (lo <= hi) {
+        // 向下取整
+        mid = ~~((lo + hi) / 2);
+        // 判断这个中间位置的高度是否大于当前开始的高度
+        if (transformedData[mid] > viewportBegin) {
+          // 如果中间值为0说明就是开始位置
+          if (mid === 0) {
+            return 0;
+          } else {
+            hi = mid - 1;
+          }
+        } else if (transformedData[mid] < viewportBegin) {
+          // 如果当前中间的高度 小于 当前开始的高度
+          if (mid + 1 < transformedData.length) {
+            if (transformedData[mid + 1] > viewportBegin) {
+              return mid;
+            } else {
+              lo = mid + 1;
+            }
+          } else {
+            return -1;
+          }
+        } else {
+          return mid;
+        }
+      }
     },
+    findBlockHeightUpperBound(viewportEnd, flxedBlockHeight) {
+      const eAdjusted = this.pageMode
+        ? viewportEnd - this.$refs.vb.scrollTop
+        : viewportEnd;
+      // 末尾的开始下标
+      const computedStartIndex = ~~(eAdjusted / flxedBlockHeight);
+      // 判断一下大于0就是正常返回否则就是返回0
+      return computedStartIndex <= this.data.length
+        ? computedStartIndex
+        : this.data.length;
+    },
+    /**
+     * 找到后面的的开始下标，但是不能有固定的高度
+     */
+    binarySearchUpperBound(viewportEnd, transformedData) {
+      let lo = 0;
+      let hi = transformedData.length - 1;
+      let mid;
+      // 二分法查找，找到末尾的下标
+      while (lo <= hi) {
+        mid = ~~((lo + hi) / 2);
+        if (transformedData[mid] > viewportEnd) {
+          if (mid > 0) {
+            if (transformedData[mid - 1] < viewportEnd) {
+              return mid;
+            } else {
+              hi = mid - 1;
+            }
+          } else {
+            return -1;
+          }
+        } else if (transformedData[mid] < viewportEnd) {
+          // 如果中间下标等于数组的长度减一
+          if (mid === transformedData.length - 1) {
+            return mid;
+          } else {
+            // 如果不等于数组长度那么我们需要加1
+            lo = mid + 1;
+          }
+        } else {
+          return mid;
+        }
+      }
+    },
+
     findBlocksInViewport(viewportBegin, viewportEnd, transformedData, data) {
       // 当视窗的开始高度小于视窗结束的高度
       if (viewportBegin < viewportEnd) {
         // 判断flxedBlockHeight是否存在
-        const lo = this.flexdBlockHeight
-          ? this.findBlockHeightLowerBound(viewportBegin, this.flexdBlockHeight)
+        // 开始下标
+        const lo = this.flxedBlockHeight
+          ? this.findBlockHeightLowerBound(viewportBegin, this.flxedBlockHeight)
           : this.binarySearchLowerBound(viewportBegin, transformedData);
+        // 结束下标
+        const hi = this.flxedBlockHeight
+          ? this.findBlockHeightUpperBound(viewportEnd, this.flxedBlockHeight)
+          : this.binarySearchUpperBound(viewportEnd, transformedData);
+        // 计算虚拟偏移量
+        let vbOffset = this.pageMode ? this.$refs.vb.offsetTop : 0;
+        // 判断是否存在固定的item的高度
+        if (this.flxedBlockHeight) {
+          this.offsetTop = lo >= 0 ? lo * this.flxedBlockHeight : 0;
+          this.offsetBot =
+            hi >= 0
+              ? (data.length - hi) * this.flxedBlockHeight : 0
+        } else {
+          this.offsetTop = lo >= 0 ? transformedData[lo] - vbOffset : 0;
+          this.offsetBot =
+            hi >= 0
+              ? transformedData[transformedData.length - 1] -
+                transformedData[hi]
+              : 0;
+        }
+        return data.slice(lo, hi);
       } else {
         this.offsetTop = 0;
         this.offsetBot = 0;
@@ -109,30 +264,16 @@ export default {
         this.transformedData,
         this.data
       );
-    },
+    }
   },
-  data() {
-    return {
-      renderList: [],
-      viewportEnd: this.height,
-      viewportBegin: 0,
-      offsetTop: 0,
-      offsetBot: 0,
-      transformedData: [],
-    };
-  },
-  watch: {
-    data: {
-      handler: (newValue, oldValue) => {},
-    },
-  },
+
   computed: {
     containerStyle() {
       return {
         ...(!this.pageMode && { height: `${this.height}px` }),
-        ...(!this.pageMode && { "overflow-y": "scroll" }),
+        ...(!this.pageMode && { "overflow-y": "scroll" })
       };
-    },
-  },
+    }
+  }
 };
 </script>
